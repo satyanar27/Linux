779cb8b96789d15d4dc77bee02eaf1a5c6133331
Satya <satyanar27@gmail.com>
2015-02-24 00:44:04 -0800

    Added satya.sh file

--- satya.sh
+++ satya.sh
@@ -1,1 +1,11 @@
-
+#!/bin/sh
+# My First Script
+#
+#clear
+#echo "Welcome"
+#echo "Top 5 Large file"
+#find . -type f -exec du -Sh {} + | sort -rh | head -n 5
+#
+echo "Top 5 large files in root"
+cd /
+sudo du -Sh | sort -rh | head -n 5
